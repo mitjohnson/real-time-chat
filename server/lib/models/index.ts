002d74595a3d862e1bd36b3501dd8d58fe4508ca
mitchell <91237766+mitjohnson@users.noreply.github.com>
2026-05-24 19:30:32 -0400

    refactor: move chatroom to models.

--- server/lib/models/index.ts
+++ server/lib/models/index.ts
@@ -1,9 +1,11 @@
 import { MessageModelFactory } from "./messages.ts";
+import { ChatRoomModel } from "./chatRoom.ts";
 import type { DatabaseSync } from 'node:sqlite';
 import type { Models } from "../../../types/index.ts";
 
 export default function models(db: DatabaseSync): Models {
   return {
-    messageModal: MessageModelFactory(db),
+    messageModel: MessageModelFactory(db),
+    chatRoomModel: ChatRoomModel(),
   }
 };
